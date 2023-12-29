5edbca4197fcb8852190cbd5cc00f7a0d4eb262f
devashishsoni98 <devashishsoni926@gmail.com>
2023-12-29 19:25:59 +0530

    sparse add

--- C/Queue.c
+++ C/Queue.c
@@ -1,54 +1,123 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// int insrt, del;
+// int f=-1, r=-1, opr;
+
+// int que[5];
+
+// void insert(){
+//     if(r==4){
+//      printf("overflow\n");
+//     }else if(r==-1 && f==-1){
+//         r=f=1;
+//         que[r]=insrt;
+//     }else{
+//         r++;
+//         que[r]=insrt;
+//     }
+// }
+
+// void delete(){
+//     if(f==-1 && r==-1){
+//         printf("underflow\n");
+//     }else if(f==r){
+//         del=que[r];
+//         r=f=-1;
+//         printf("%d is deleted.\n", del);
+//     }else{
+//         del = que[f];
+//         f++;
+//         printf("%d is deleted", del);
+//     }
+// }
+//  void display (){
+//     if(r==-1 && f==-1){
+//         printf("khali\n");
+//     }else{
+//         printf("queu ye h:\n");
+//         for(int i =f; i<=r;i++){
+//             printf("|%d|", que[i]);
+//         }
+//         printf("\n");
+//     }
+//  }
+
+// int main(){
+
+//     while(1){
+//         printf("1,2,3 ya 4 dalo:");
+//         scanf("%d", &opr);
+//         switch(opr){
+//             case 1:
+//             printf("value dalo:");
+//             scanf("%d", &insrt);
+//             insert();
+//             break;
+//             case 2:
+//             delete();
+//             break;
+//             case 3:
+//             display();
+//             break;
+//             case 4: 
+//             printf("khatam\n");
+//             exit(0);
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+
 #include<stdio.h>
 #include<stdlib.h>
+
 int insrt, del;
-int f=-1, r=-1, opr;
+int f=-1;
+int r=-1;
 
 int que[5];
 
-void insert(){
+void insert (){
     if(r==4){
-     printf("overflow\n");
+        printf("Overflow\n");
     }else if(r==-1 && f==-1){
         r=f=1;
         que[r]=insrt;
-    }else{
+    }else {
         r++;
         que[r]=insrt;
     }
 }
-
-void delete(){
-    if(f==-1 && r==-1){
-        printf("underflow\n");
+void delete (){
+    if(r==-1 && f==-1){
+        printf("Underflow\n");
     }else if(f==r){
         del=que[r];
         r=f=-1;
-        printf("%d is deleted.\n", del);
-    }else{
+        printf("The deleted element is : %d\n",del);
+    }else {
         del = que[f];
         f++;
-        printf("%d is deleted", del);
+        printf("The deleted element is : %d\n",del);
     }
 }
- void display (){
-    if(r==-1 && f==-1){
-        printf("khali\n");
-    }else{
-        printf("queu ye h:\n");
-        for(int i =f; i<=r;i++){
-            printf("|%d|", que[i]);
-        }
-        printf("\n");
+
+void display (){
+    printf("queue ye hai:\n");
+    for(int i=f;i<=r;i++){
+        printf("|%d|", que[i]);
     }
- }
+    printf("\n");
+}
 
-int main(){
-
-    while(1){
-        printf("1,2,3 ya 4 dalo:");
-        scanf("%d", &opr);
-        switch(opr){
-            case 1:
+int main () {
+   int opr;
+      while(1){
+   printf("enter opr");
+   scanf("%d",&opr);
+    switch(opr){
+        case 1:
             printf("value dalo:");
             scanf("%d", &insrt);
             insert();
@@ -59,11 +128,10 @@ int main(){
             case 3:
             display();
             break;
-            case 4: 
-            printf("khatam\n");
+            case 4:
             exit(0);
             break;
-        }
     }
+   }
     return 0;
 }
