e41ae36b21a64ec01bf2ab123db6023d85e41173
devashishsoni98 <devashishsoni926@gmail.com>
2024-02-29 21:07:39 +0530

    tailwind setup

---
+++ Tailwind/my-project/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App h-screen grid place-content-center bg-slate-900">
+      <h1 className="text-3xl font-bold underline text-white">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
