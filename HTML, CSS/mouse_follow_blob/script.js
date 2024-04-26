80b02e0d1821c79f34cafabd92b60d4af5a5f35a
devashishsoni98 <devashishsoni926@gmail.com>
2024-04-26 17:13:41 +0530

    blob

---
+++ HTML, CSS/mouse_follow_blob/script.js
@@ -0,0 +1,12 @@
+const blob = document.getElementById("blob");
+
+document.body.onpointermove = event => {
+    const { clientX,clientY } = event;
+
+    // blob.style.left = `${clientX}px`;
+    // blob.style.top = `${clientY}px`;
+    blob.animate({
+        left: `${clientX}px`,
+        top:`${clientY}px` 
+    },{ duration:20, fill:"forwards" });
+}
---
+++ React/test-app/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div>
+        <center><h2 className='comfortaa-blah'>Simple Projects Surge</h2></center>
+    </div>
+  )
+}
+
+export default Home
