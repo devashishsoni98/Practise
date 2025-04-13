c5d3be4236fa460f2bcb969cbd241336a5e5f6e1
devashishsoni98 <devashishsoni926@gmail.com>
2025-04-13 23:57:30 +0530

    optimized hashmap

--- C++/questions/twosum.cpp
+++ C++/questions/twosum.cpp
@@ -1,21 +1,63 @@
 // two sum
 // brute force
 
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+// using namespace std;
+
+// vector<int> solution(vector<int> &nums, int target)
+// {
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         for (int j = i + 1; j <= nums.size(); j++)
+//         {
+//             if (nums[i] + nums[j] == target)
+//             {
+//                 return {i, j};
+//             }
+//         }
+//     }
+//     return {-1, -1};
+// }
+// int main()
+// {
+//     vector<int> sample = {1, 2, 3, 4, 5};
+//     int tar = 7;
+//     vector<int> res;
+//     res = solution(sample, tar);
+//     if (res[0] != -1)
+//     {
+//         cout << res[0] << ", " << res[1];
+//     }
+//     else
+//     {
+//         cout << "not found";
+//     }
+//     return 0;
+// }
+
+// optimized hasmap
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 using namespace std;
 
+unordered_map<int, int> mymap;
+
 vector<int> solution(vector<int> &nums, int target)
 {
     for (int i = 0; i < nums.size(); i++)
     {
-        for (int j = i + 1; j <= nums.size(); j++)
+        int comp = target - nums[i];
+        if (mymap.find(comp) != mymap.end())
         {
-            if (nums[i] + nums[j] == target)
-            {
-                return {i, j};
-            }
+            return {mymap[comp], i};
+        }
+        else
+        {
+            mymap[nums[i]] = i;
         }
     }
     return {-1, -1};
